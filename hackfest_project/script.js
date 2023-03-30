ffc4daaa0ed752127d7fca863a7722b24213e3a6
Ankush Yadav <yadavankush2404@gmail.com>
2023-03-30 20:25:07 +0530

    Trying to commit folder hackfest from folderout pwd

---
+++ hackfest_project/script.js
@@ -0,0 +1,12 @@
+var b=0;
+function btn(){
+    b++;
+    if(b==1)
+        alert("Did not it said \"Do not click it\" ")
+    else if(b==2)
+        alert("Are you not able to read instruction");
+    else if(b==3)
+        alert("This proves you are a dump person");
+    else if (b>3)
+        alert("I understand you have never seen a button");
+}
